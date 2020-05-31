8716dcf242097c18aefeb7e4db5f954572ca18ce
kyle Dornan <kyledornan26@gmail.com>
2020-05-31 15:34:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
-def line(num)
-  if num = 0
+def line(katz_deli)
+  if katz_deli == 0
 puts "The line is currently empty.".
-
-
-
+else 
+  message = "The line is currently:"
+  katz_deli.each_with_index do 
   end 
